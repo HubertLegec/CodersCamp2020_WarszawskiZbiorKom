1feab735fe1b937b937cc372b4b91b6953620768
brudek <brudek@op.pl>
2020-12-28 23:57:56 +0100

    create all stops names list and search input

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,28 @@
 export const App = ({options}) => {
+    const stopsList = [];
+    let uniqueStopsList = [];
+    const url = `${options.wawApiBaseUrl}${options.wawApiAllStops}&apikey=${options.wawApiKey}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => stopsList.push(...data.result))
+    .then(() => {
+        for(let stop of stopsList){ 
+            // place in api with bus/tram stop name  
+            let stopName = stop['values'][2]['value'];
+            // checking for unique names
+            uniqueStopsList.indexOf(stopName) < 0 ? uniqueStopsList.push(stopName) : false;
+        }
+    })
+    .then(() => uniqueStopsList = uniqueStopsList.sort((a, b) => a.localeCompare(b)))
+    .then(() => {
+        // html datalist element
+        const allStops = document.getElementById('AllStops');
+        // creating datalist options
+        for(let unique of uniqueStopsList){
+            let option = document.createElement('option');
+            option.value = unique;
+            allStops.append(option);
+        }
+    });   
 }
-
-
