39ba993ecb80f616e120c5ebfcc9bb8e6de58809
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-04 19:49:20 +0100

    CreateStopsList function refactoring

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,21 @@
 export const App = ({options}) => {
     const stopsList = [];
-    let uniqueStopsList = [];
+    const uniqueStopsList = [];
     const url = `${options.wawApiBaseUrl}${options.wawApiAllStops}&apikey=${options.wawApiKey}`;
 
+    function createDatalist(datalistId, arrayOfOptions) {
+        const datalist = document.getElementById(datalistId);
+
+        // creating datalist options
+        for(let element of arrayOfOptions){
+            let option = document.createElement('option');
+            option.value = element;
+            datalist.append(option);
+        }
+    }
+
     async function createStopsList() {
+        
         const response = await fetch(url);
         const data = await response.json();
         
@@ -12,20 +24,12 @@ export const App = ({options}) => {
         for(let stop of stopsList){ 
             // place in api with bus/tram stop name  
             let stopName = stop['values'][2]['value'];
-
             // checking for unique names
             uniqueStopsList.indexOf(stopName) < 0 ? uniqueStopsList.push(stopName) : false;            
         }
 
-        uniqueStopsList = uniqueStopsList.sort((a, b) => a.localeCompare(b));
-
-        const allStops = document.getElementById('AllStops');
-        // creating datalist options
-        for(let unique of uniqueStopsList){
-            let option = document.createElement('option');
-            option.value = unique;
-            allStops.append(option);
-        }
+        uniqueStopsList.sort((a, b) => a.localeCompare(b));
+        createDatalist('AllStops', uniqueStopsList);
     }
 
     createStopsList();
