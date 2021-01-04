ec69555f8b3e859e8dda961fd2208e63392169f0
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-04 19:14:27 +0100

    Rewritten promise code with async/await

--- src/app/App.js
+++ src/app/App.js
@@ -3,20 +3,22 @@ export const App = ({options}) => {
     let uniqueStopsList = [];
     const url = `${options.wawApiBaseUrl}${options.wawApiAllStops}&apikey=${options.wawApiKey}`;
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => stopsList.push(...data.result))
-    .then(() => {
+    async function createStopsList() {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        stopsList.push(...data['result']);
+
         for(let stop of stopsList){ 
             // place in api with bus/tram stop name  
             let stopName = stop['values'][2]['value'];
+
             // checking for unique names
-            uniqueStopsList.indexOf(stopName) < 0 ? uniqueStopsList.push(stopName) : false;
+            uniqueStopsList.indexOf(stopName) < 0 ? uniqueStopsList.push(stopName) : false;            
         }
-    })
-    .then(() => uniqueStopsList = uniqueStopsList.sort((a, b) => a.localeCompare(b)))
-    .then(() => {
-        // html datalist element
+
+        uniqueStopsList = uniqueStopsList.sort((a, b) => a.localeCompare(b));
+
         const allStops = document.getElementById('AllStops');
         // creating datalist options
         for(let unique of uniqueStopsList){
@@ -24,5 +26,8 @@ export const App = ({options}) => {
             option.value = unique;
             allStops.append(option);
         }
-    });   
+    }
+
+    createStopsList();
+
 }
