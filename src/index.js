3aea56ea3c9a837bc3b94fbd77e91e0c25fe414f
esdawid <starzyk.dawid.0826@gmail.com>
2021-01-05 18:49:36 +0100

    PRINT LINES USING ID OF BUS NAME IN INPUT #2

--- src/index.js
+++ src/index.js
@@ -6,34 +6,57 @@
 
 // window.onload = () => App({options: {wawApiBaseUrl: WAW_API_BASE_URL}})
 
-let busStopId = window.prompt("Podaj identyfikator przystanku:");
-let busStopNr = window.prompt("Podaj identyfikator słupka:");
+let stopId;
+let stopNr;
 let stopName;
-const input = document.getElementById('FindStop')
-input.addEventListener('keyup', () => {
-    stopName = input.value
-})
-    const endpoint =
-    `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=88cd555f-6f31-43ca-9de4-66c479ad5942&busstopId=${busStopId}&busstopNr=${busStopNr}&apikey=213a69a2-30fb-4d1e-b819-aa1b5c02f3c6`;
+const input = document.getElementById("FindStop");
+const btn = document.getElementById("searchStop");
 
-              
-    fetch(endpoint)
-        .then(response => response.ok ? response.json() : undefined)
+btn.addEventListener("click", () => {
+  getBusDataFromInput();
+  removeElementsByClass('elementOfList');
+});
+
+function getBusDataFromInput() {
+  stopName = encodeURI(input.value.slice(0, input.value.length - 3));
+  stopNr = input.value.slice(input.value.length - 2, input.value.length);
+  const endPoint = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=${stopName}&apikey=66b60069-cff2-418b-a9a9-4a64cfc17443`;
+  fetch(endPoint)
+    .then(response => response.json())
+    .then(data => [...data.result])
+    .then(data => data[0].values[0].value)
+    .then(id => {
+      const endpoint = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=88cd555f-6f31-43ca-9de4-66c479ad5942&busstopId=${id}&busstopNr=${stopNr}&apikey=213a69a2-30fb-4d1e-b819-aa1b5c02f3c6`;
+
+
+      fetch(endpoint)
+        .then(response => (response.ok ? response.json() : undefined))
         .then(data => [...data.result])
         .then(array => array.map(el => el.values[0].value))
-        .then(data => data.forEach(element =>{
-            let para = document.createElement("P");
-            para.classList.add("elementOfList")
-            if(element > 100 || element[0] === "N"){
-                para.innerHTML = element + ` autobus`
-                document.getElementById("busList").appendChild(para)
-            }else {
-                para.innerHTML = element + ` tramwaj`
-                document.getElementById("tramList").appendChild(para)
-            }
-        }))
-        .catch(errorMessage => console.log(errorMessage));
+        .then(data =>
+          data.forEach(element => {
 
+            let para = document.createElement("P");
+            para.classList.add("elementOfList");
+            if (element > 100 || element[0] === "N") {
+              para.innerHTML = element;
+              document.getElementById("busList").appendChild(para);
+            } else {
+              para.innerHTML = element;
+              document.getElementById("tramList").appendChild(para);
+            }
+          })
+        )
+        .catch(errorMessage => console.log(errorMessage));
+    });
+}
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
             
 
