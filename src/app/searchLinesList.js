9925dd5c2858111f729ec23f2e6d9e6195651cb4
esdawid <starzyk.dawid.0826@gmail.com>
2021-01-09 10:35:46 +0100

    REFACTOR CODE AND ADD TEST
    Split code into several functions and add single test

--- src/app/searchLinesList.js
+++ src/app/searchLinesList.js
@@ -1,48 +1,54 @@
-
 export const linesList = (api) => {
     const apiKey = api;
-    let stopNr;
-    let stopName;
-    const input = document.getElementById("FindStop");
-    const btn = document.getElementById("searchStop");
-
     btn.addEventListener("click", () => {
-    getBusDataFromInput();
-    removeElementsByClass('elementOfList');
+        removeElementsByClass('elementOfList')
+        getBusDataFromInput(apiKey)
     });
+}
 
-    function getBusDataFromInput() {
-    stopName = encodeURI(input.value.slice(0, input.value.length - 3));
-    stopNr = input.value.slice(input.value.length - 2, input.value.length);
-    const endPoint = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=${stopName}&apikey=${apiKey}`;
-    fetch(endPoint)
-        .then(response => response.json())
-        .then(data => [...data.result])
-        .then(data => data[0].values[0].value)
-        .then(id => {
+const input = document.getElementById("FindStop");
+const btn = document.getElementById("searchStop");
+    async function getBusDataFromInput(apiKey) {
+        let stopName = encodeURI(input.value.slice(0, input.value.length - 3));
+        let stopNr = input.value.slice(input.value.length - 2, input.value.length);
+        const endPoint = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=${stopName}&apikey=${apiKey}`;
+        try{
+            const result = await fetch(endPoint)
+            const data = await result.json();
+            getLines(data.result[0].values[0].value, stopNr, apiKey) //run new function
+            return data.result[0].values[0].value;
+        }
+        catch (e) {
+            return null
+        }
+    }
+
+    async function getLines(id, stopNr, apiKey){
+        let arr = []
         const endpoint = `https://api.um.warszawa.pl/api/action/dbtimetable_get/?id=88cd555f-6f31-43ca-9de4-66c479ad5942&busstopId=${id}&busstopNr=${stopNr}&apikey=${apiKey}`;
+        try{
+            const result = await fetch(endpoint)
+            const data = await result.json();
+            let arr = data.result.map(el => el.values[0].value)
+            displayLines(arr) // run new function
+            return data.result[0].values[0].value;
+            }
+        catch (e){
+            return null
+        }                            
+    }
 
-
-        fetch(endpoint)
-            .then(response => (response.ok ? response.json() : undefined))
-            .then(data => [...data.result])
-            .then(array => array.map(el => el.values[0].value))
-            .then(data =>
-            data.forEach(element => {
-
-                let para = document.createElement("P");
-                para.classList.add("elementOfList");
-                if (element > 100 || element[0] === "N") {
-                para.innerHTML = element;
-                document.getElementById("busList").appendChild(para);
-                } else {
-                para.innerHTML = element;
-                document.getElementById("tramList").appendChild(para);
-                }
-            })
-            )
-            .catch(errorMessage => console.log(errorMessage));
-        });
+    function displayLines(arr){
+        arr.forEach(element => {
+            let para = document.createElement("P");
+            para.classList.add("elementOfList");
+            para.innerHTML = element;
+            if (element > 100 || element[0] === "N") {
+            document.getElementById("busList").appendChild(para);
+            } else {
+            document.getElementById("tramList").appendChild(para);
+            }
+        })
     }
 
     function removeElementsByClass(className){
@@ -51,8 +57,6 @@ export const linesList = (api) => {
             elements[0].parentNode.removeChild(elements[0]);
         }
     }
-}         
+export {getBusDataFromInput};
+export {getLines};
 
-              
-
-              
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime' //async/await with Parcel
-import {linesList} from "./app/searchLinesList";
+import {linesList, stopNr} from "./app/searchLinesList";
 const WAW_API_BASE_URL = "https://api.um.warszawa.pl/api/" || process.env.WAW_API_BASE_URL;
 const API_KEY = '213a69a2-30fb-4d1e-b819-aa1b5c02f3c6';
 
