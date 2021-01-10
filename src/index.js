d84d47af2e1675839758c9fe9a28bbbfa8fb7837
Tomasz Massalski <tomasz.massalski@gmail.com>
2021-01-10 17:59:47 +0100

    Adds map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import 'regenerator-runtime/runtime' //async/await with Parcel
 import {App} from "./app/App";
+import {loadMap} from "./app/Map";
 
 const WAW_API_BASE_URL = process.env.WAW_API_BASE_URL || "https://api.um.warszawa.pl/api";
 
 window.onload = () => App({options: {wawApiBaseUrl: WAW_API_BASE_URL}})
+
+loadMap();
