d72af14e4d07b3cb3692ab58ad07b817e5fa81ca
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-10 22:57:29 +0100

    Refactored createSortedStopsDatalist func

--- src/app/DOMModifier.js
+++ src/app/DOMModifier.js
@@ -1,16 +1,17 @@
 export class DOMModifier {
 
-    createSortedStopsDatalist(datalistId, stopsList) {
+    createSortedStopsDatalist(datalistId, stopList) {
             // creating datalist options
-            const datalist = document.getElementById(datalistId);
-            stopsList.sort((a, b) => `${a.name} ${a.number}`.localeCompare(`${b.name} ${b.number}`));
+        const datalist = document.getElementById(datalistId);
 
-            for (let element of stopsList) {
+        stopList.map(s => `${s.name} ${s.number}`)
+        .sort((s1, s2) => s1.localeCompare(s2))
+        .map(s => {
             const option = document.createElement('option');
-            option.value = `${element.name} ${element.number}`;
-            datalist.append(option);
-        }
-        
+            option.value = s;
+            return option;
+        })
+        .forEach(opt => datalist.append(opt));
     }
-
+        
 }
