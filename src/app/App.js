b6893c21c4ba0c051897181cea9ecc2f0ef6d87a
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-10 22:56:47 +0100

    App now awaits getStops func

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import {ApiClient} from './ApiClient';
 import {StorageManager} from './StorageManager';
 import {DOMModifier} from './DOMModifier';
-export const App = ({options}) => {
+export const App = async ({options}) => {
     const storage = new StorageManager();
     const apiClient = new ApiClient(options['wawApiBaseUrl']);
     const domModifier = new DOMModifier();
 
-    apiClient.getStops(`${options['wawApiAllStops']}${options['wawApiKey']}`).then(result => storage.storeData('stopsList', result));
+    const result = await apiClient.getStops(`${options['wawApiAllStops']}${options['wawApiKey']}`);
+    storage.storeData('stopsList', result);   
     domModifier.createSortedStopsDatalist('AllStops', storage.getData('stopsList'));
-
 }
