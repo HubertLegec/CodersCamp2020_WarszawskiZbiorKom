24895fc41da85acf7818c02b5d2550f04fa69dfc
EsDawid <48258322+EsDawid@users.noreply.github.com>
2021-01-12 17:11:50 +0100

    delete unnecesary properties
    delete unnecesary properties

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,7 @@ import {DOMModifier} from './DOMModifier';
 export const App = async ({options}) => {
     const storage = new StorageManager();
     const apiClient = new ApiClient(options['wawApiBaseUrl']);
-    const domModifier = new DOMModifier(options.wawApiKey, options.wawApiBaseUrl);
+    const domModifier = new DOMModifier();
     const displayLinesList = new displayTransportLines(options.wawApiKey, options.wawApiBaseUrl, options['wawApiGetId'])
     const result = await apiClient.getStops(`${options['wawApiAllStops']}${options['wawApiKey']}`);
     storage.storeData('stopsList', result);   
