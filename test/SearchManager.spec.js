aa13a8b90d1b3bac62341b5229ee0ab208f0a24d
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-14 20:08:18 +0100

    Added createInput tests

--- test/SearchManager.spec.js
+++ test/SearchManager.spec.js
@@ -1,12 +1,45 @@
-//import {SearchManager} from '../src/app/SearchManager';
-import '@testing-library/jest-dom/extend-expect';
+import { SearchManager } from '../src/app/SearchManager';
+import { getByRole, getAllByRole, waitFor, fireEvent, getAllByText } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import 'jest-localstorage-mock';
+import { Stop } from '../src/app/Stop';
 
 //przy imporcie searchmanagera jest błąd 'Missing class properties transform'
-describe('Tests input exists and values', () => {
-    test('nie umiem pisać testów', () =>{
-        //const searchManager = new SearchManager();
-        expect(true).toBe(true);
-        expect(true).toBeTruthy();
-        expect(true).not.toBeFalsy();
+describe('SearchManager testing', () => {
+
+    function createContainerForInput() {
+            const div = document.createElement('div');
+            div.id = 'zbiorkom-app';
+            
+            return div;
+    }
+    
+    describe('createInput testing', () => {
+
+        beforeEach(() => {
+            document.body.innerHTML = '';
+        })
+
+        test('creates input into div', () => {
+            const container = document.body;
+            container.append(createContainerForInput());
+            const searchManager = new SearchManager('zbiorkom-app');
+
+            searchManager.createInput();
+
+            expect(getByRole(container, 'textbox')).toBeInTheDocument();
+        })
+
+        test('created input id equals "autoComplete"', () => {
+            const container = document.body;
+            container.append(createContainerForInput());
+            const searchManager = new SearchManager('zbiorkom-app');
+
+            searchManager.createInput();
+
+            expect(getByRole(container, 'textbox').id).toEqual('autoComplete');
+        })
+
     })
+    
 })
