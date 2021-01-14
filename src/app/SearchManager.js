6e74a5879e95ace865ab13406e6b7dc898f4335f
Hubert Legęć <hubert.legec@gmail.com>
2021-01-14 23:33:37 +0100

    suggestions test for SearchManager

--- src/app/SearchManager.js
+++ src/app/SearchManager.js
@@ -1,120 +1,102 @@
 import AutoComplete from '../../node_modules/@tarekraafat/autocomplete.js/dist/js/autoComplete';
-import {StorageManager} from './StorageManager';
+import { StorageManager } from './StorageManager';
 
-
-/*let input = document.createElement('input');
-input.id = 'autoComplete';
-input.type = 'text';
-
-input.addEventListener("results", (event) => {
-    return event;
-});
-
-document.querySelector('#zbiorkom-app').append(input);*/
 
 export class SearchManager {
 
-    constructor(InputContainerId) {
-        this.InputContainerId = InputContainerId;
-    }
+  constructor(inputContainerId, getStops) {
+    this.inputContainerId = inputContainerId;
+    this.getStops = getStops;
+  }
 
-    createInput() {
-        let input = document.createElement('input');
-        input.id = 'autoComplete';
-        input.type = 'text';
+  createInput() {
+    const input = document.createElement('input');
+    input.id = 'autoComplete';
+    input.type = 'text';
 
-        input.addEventListener("results", (event) => { return event });
+    document.getElementById(this.inputContainerId).append(input);
+    this.autoComplete();
+    return input;
+  }
 
-        document.getElementById(this.InputContainerId).append(input);
-    }
-
-    // The autoComplete.js Engine instance creator
-    autoComplete() { 
-        return new AutoComplete({
-        name: "Stops",
-        data: {
-            src: async function () {
-                
-                // Loading placeholder text
-                document
-                    .querySelector("#autoComplete")
-                    .setAttribute("placeholder", "Znajdź przystanek");
-                
-                const storageManager = new StorageManager();
-                const storageData = storageManager.getData('stopsList');
-                let data = await storageData
-                .map(s => s = {...s, fullName : `${s.name} ${s.number}`})
-                .sort((s1, s2) => s1.fullName.localeCompare(s2.fullName))
-                // Returns Fetched data
-                return data;
-            },
-            key: ["fullName"],
-            results: (list) => {
-                // Filter duplicates
-                const filteredResults = Array.from(
-                    new Set(list.map((value) => value.match))
-                ).map((stop) => {
-                    return list.find((value) => value.match === stop);
-                });
-                return filteredResults;
-            }
+  // The autoComplete.js Engine instance creator
+  autoComplete() {
+    return new AutoComplete({
+      name: 'Stops',
+      data: {
+        src: async () => {
+          // Loading placeholder text
+          document
+            .querySelector('#autoComplete')
+            .setAttribute('placeholder', 'Znajdź przystanek');
+          return this.getStops()
+            .map(s => ({ ...s, fullName: `${s.name} ${s.number}` }))
+            .sort((s1, s2) => s1.fullName.localeCompare(s2.fullName));
         },
-        trigger: {
-            event: ["input", "focus"]
+        key: ['fullName'],
+        results: (list) => {
+          // Filter duplicates
+          const filteredResults = Array.from(
+            new Set(list.map((value) => value.match)),
+          ).map((stop) => {
+            return list.find((value) => value.match === stop);
+          });
+          return filteredResults;
         },
-        query: {                             
-            manipulate: (query) => {
-                // remove polish accents/diacritics
-                return query//.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-            }
+      },
+      trigger: {
+        event: ['input', 'focus'],
+      },
+      query: {
+        manipulate: (query) => {
+          // remove polish accents/diacritics
+          return query;//.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
         },
-        cache: true,
-        placeHolder: "Znajdź przystanek",
-        threshold: 1,
-        searchEngine: "strict", // "loose"
-        highlight: true,
-        maxResults: 1000,
-        diacritics: false,
-        resultItem: {
-            content: (data, element) => {
-                // Prepare Value's Key
-                const key = Object.keys(data.value).find(
-                    (key) => data.value[key] === element.innerText
-                );
-                // Modify Results Item
-                element.style = "display: flex; justify-content: space-between;";
-                element.innerHTML = `<span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+      },
+      cache: true,
+      placeHolder: 'Znajdź przystanek',
+      threshold: 1,
+      searchEngine: 'strict', // "loose"
+      highlight: true,
+      maxResults: 1000,
+      diacritics: false,
+      resultItem: {
+        content: (data, element) => {
+          // Prepare Value's Key
+          const key = Object.keys(data.value).find(
+            (key) => data.value[key] === element.innerText,
+          );
+          // Modify Results Item
+          element.style = 'display: flex; justify-content: space-between;';
+          element.innerHTML = `<span style='text-overflow: ellipsis; white-space: nowrap; overflow: hidden;'>
                 ${element.innerHTML}</span>
-                <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
+                <span style='display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);'>
                 Nazwa przystanku</span>`;
-            }
         },
-        noResults: (dataFeedback, generateList) => {
-            // Generate autoComplete List
-            generateList(this.searchManager, dataFeedback, dataFeedback.results);
-            // No Results List Item
-            const result = document.createElement("li");
-            result.setAttribute("class", "no_result");
-            result.setAttribute("tabindex", "1");
-            result.innerHTML = `<span>Brak wyników dla zapytania "${dataFeedback.query}"</span>`;
-            document
-                .querySelector(`#${this.searchManager.resultsList.idName}`)
-                .appendChild(result);
-        },
-        onSelection: (feedback) => {
-            document.querySelector("#autoComplete").blur();
-            // Prepare User's Selected Value
-            const selection = feedback.selection.value[feedback.selection.key];
-            // Render selected choice to selection div
-            // document.querySelector(".selected-stop").innerHTML = selection;
-            // Replace Input value with the selected value
-            document.querySelector("#autoComplete").value = selection;
-            // Console log autoComplete data feedback
-            console.log(feedback.selection.value);
-        }
+      },
+      noResults: (dataFeedback, generateList) => {
+        // Generate autoComplete List
+        generateList(this.searchManager, dataFeedback, dataFeedback.results);
+        // No Results List Item
+        const result = document.createElement('li');
+        result.setAttribute('class', 'no_result');
+        result.setAttribute('tabindex', '1');
+        result.innerHTML = `<span>Brak wyników dla zapytania "${dataFeedback.query}"</span>`;
+        document
+          .querySelector(`#${this.searchManager.resultsList.idName}`)
+          .appendChild(result);
+      },
+      onSelection: (feedback) => {
+        document.querySelector('#autoComplete').blur();
+        // Prepare User's Selected Value
+        const selection = feedback.selection.value[feedback.selection.key];
+        // Render selected choice to selection div
+        // document.querySelector(".selected-stop").innerHTML = selection;
+        // Replace Input value with the selected value
+        document.querySelector('#autoComplete').value = selection;
+        // Console log autoComplete data feedback
+        console.log(feedback.selection.value);
+      },
     });
-}
-    
-
-
+  }
 }
--- test/SearchManager.spec.js
+++ test/SearchManager.spec.js
@@ -1,45 +1,61 @@
 import { SearchManager } from '../src/app/SearchManager';
-import { getByRole, getAllByRole, waitFor, fireEvent, getAllByText } from '@testing-library/dom';
+import { getByRole, waitFor, fireEvent, queryByText, findByText } from '@testing-library/dom';
 import '@testing-library/jest-dom';
-import 'jest-localstorage-mock';
-import { Stop } from '../src/app/Stop';
 
 //przy imporcie searchmanagera jest błąd 'Missing class properties transform'
-describe('SearchManager testing', () => {
+describe('SearchManager', () => {
+  let container;
 
-    function createContainerForInput() {
-            const div = document.createElement('div');
-            div.id = 'zbiorkom-app';
-            
-            return div;
+  function createContainerForInput() {
+    const div = document.createElement('div');
+    div.id = 'zbiorkom-app';
+    return div;
+  }
+
+  beforeEach(() => {
+    document.body.innerHTML = '';
+    container = createContainerForInput();
+    document.body.append(container);
+  });
+
+  test('creates input into div', () => {
+    const searchManager = new SearchManager('zbiorkom-app', () => []);
+
+    searchManager.createInput();
+
+    expect(getByRole(container, 'textbox')).toBeInTheDocument();
+  });
+
+  test('created input id equals "autoComplete"', () => {
+    const searchManager = new SearchManager('zbiorkom-app', () => []);
+
+    searchManager.createInput();
+
+    expect(getByRole(container, 'textbox').id).toEqual('autoComplete');
+  });
+
+  test('displays suggestions', async () => {
+    const availableStops = [
+      { name: 'Marszałkowska', number: '01' },
+      { name: 'Puławska', number: '02' },
+    ];
+    const searchManager = new SearchManager('zbiorkom-app', () => availableStops);
+    const input = searchManager.createInput();
+
+    fireEvent.focus(input);
+    fireEvent.input(input, { target: { value: 'Ma' } });
+
+    const matchingStop = await findByText(container, containsIgnoringHtmlTags('Marszałkowska 01'));
+    expect(matchingStop).toBeInTheDocument();
+    await waitFor(() => expect(queryByText(container, containsIgnoringHtmlTags('Puławska 02'))).not.toBeInTheDocument());
+  });
+
+  function containsIgnoringHtmlTags(text) {
+    return (content, node) => {
+      const hasText = node => node.textContent.indexOf(text) >= 0;
+      const childrenDontHaveText = Array.from(node.children)
+        .every(child => !hasText(child));
+      return hasText(node) && childrenDontHaveText;
     }
-    
-    describe('createInput testing', () => {
-
-        beforeEach(() => {
-            document.body.innerHTML = '';
-        })
-
-        test('creates input into div', () => {
-            const container = document.body;
-            container.append(createContainerForInput());
-            const searchManager = new SearchManager('zbiorkom-app');
-
-            searchManager.createInput();
-
-            expect(getByRole(container, 'textbox')).toBeInTheDocument();
-        })
-
-        test('created input id equals "autoComplete"', () => {
-            const container = document.body;
-            container.append(createContainerForInput());
-            const searchManager = new SearchManager('zbiorkom-app');
-
-            searchManager.createInput();
-
-            expect(getByRole(container, 'textbox').id).toEqual('autoComplete');
-        })
-
-    })
-    
-})
+  }
+});
