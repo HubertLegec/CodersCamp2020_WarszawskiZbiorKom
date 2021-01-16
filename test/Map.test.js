67a6171e285715c381056f6fac0cf94265a1875a
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-01-16 16:31:46 +0100

    ApiClient and Map tests

---
+++ test/Map.test.js
@@ -0,0 +1,20 @@
+import { Map } from '../src/app/Map';
+
+describe('Map testing', () => {
+
+    beforeEach(() => {
+        document.body.innerHTML = ''; 
+    })
+
+    test('initializeMap creates map inside div with "map" id', () => {
+        const div = document.createElement('div');
+        div.id = 'map';
+        document.body.append(div);
+        const map = new Map();
+        const mapContainer = document.getElementById('map');
+
+        map.initializeMap();
+
+        expect(mapContainer.firstChild.classList.contains('leaflet-map-pane')).toBeTruthy();
+    })
+})
