996b0894f4378df07155d92f534db46999834bf7
Hubert Legęć <hubert.legec@gmail.com>
2021-01-17 23:02:29 +0100

    Regenerate package-lock.json with Node v14

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'regenerator-runtime/runtime';
+import 'regenerator-runtime/runtime'; //async/await with Parcel
 import {App} from './app/App';
 
 const WAW_API_BASE_URL = 'https://api.um.warszawa.pl/api/' || process.env.WAW_API_BASE_URL;
