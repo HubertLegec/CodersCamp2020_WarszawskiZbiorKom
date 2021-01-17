afdc1e6e78d90374a8f1e057db6b9c6ca08b1a75
tmassalski <tomasz.massalski@gmail.com>
2021-01-17 14:20:47 +0100

    Adds map (#12)
    * Adds map
    
    * Adds missing map.js
    
    * Search input
    
    * Adds map
    
    * Adds missing map.js
    
    * rebasing, dependencies moved to NPM
    
    * Adds missing stylesheet and module
    
    * adds @babel/plugin-proposal-class-properties
    
    * yet another fix of babel
    
    * Display lines for selected stop
    
    * Remove unneeded @babel/plugin-proposal-class-properties
    
    * Adds map
    
    * Adds missing map.js
    
    * rebasing, dependencies moved to NPM
    
    * Adds missing stylesheet and module
    
    * Adds map
    
    * Adds missing map.js
    
    * adds @babel/plugin-proposal-class-properties
    
    * yet another fix of babel
    
    * Remove unneeded @babel/plugin-proposal-class-properties
    
    * map container height change
    
    Co-authored-by: mbr95 <64269745+mbr95@users.noreply.github.com>
    Co-authored-by: EsDawid <48258322+EsDawid@users.noreply.github.com>
    Co-authored-by: Hubert Legęć <hubert.legec@gmail.com>

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import {ApiClient} from './ApiClient';
 import {StorageManager} from './StorageManager';
 import {StopLinesManager} from './StopLinesManager';
+import {Map} from "./Map";
+
 export const App = async ({options}) => {
     const storage = new StorageManager();
     const apiClient = new ApiClient(options['wawApiBaseUrl'], options['wawApiKey']);
+    const map = new Map();
+    
     const result = await apiClient.getStops(`${options['wawApiAllStops']}${options['wawApiKey']}`);
+
     storage.storeData('stopsList', result);   
     const obj = {
         id: "7013",
@@ -14,4 +19,5 @@ export const App = async ({options}) => {
     const listOfLines = await apiClient.getLines(obj.id, obj.stopNr);
     const stopLinesManager = new StopLinesManager('zbiorkom-app', listOfLines);
     stopLinesManager.createButton();
+    map.initializeMap();
 }
