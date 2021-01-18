1ae254ebfee1ad1ab41cb3efe048f3b77eb0940a
Tomasz Massalski <tomasz.massalski@gmail.com>
2021-01-18 20:14:29 +0100

    Move map zoom control to bottom right

--- src/app/MapManager.js
+++ src/app/MapManager.js
@@ -5,7 +5,8 @@ export class MapManager {
     
     initializeMap(){
         const L = require('leaflet');
-        const map = L.map('map').setView([52.2297700, 21.0117800], 12);
+        const map = L.map('map',{ zoomControl: false }).setView([52.2297700, 21.0117800], 12);
+        L.control.zoom({position: 'bottomright'}).addTo(map);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
