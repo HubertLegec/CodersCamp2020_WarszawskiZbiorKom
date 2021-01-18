e8f77dc08bd9cb031c5125135fcfe4aef4b7cf9f
Hubert Legęć <hubert.legec@gmail.com>
2021-01-18 17:36:54 +0100

    comment out all code - to test if 'regenerator-runtime'

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,3 @@
-import {ApiClient} from './ApiClient';
-import {StorageManager} from './StorageManager';
-import {StopLinesManager} from './StopLinesManager';
-import {Map} from "./Map";
-import {SearchManager} from './SearchManager';
-
 export const App = ({options}) => {
-    const storage = new StorageManager();
-    const apiClient = new ApiClient(options['wawApiBaseUrl'], options['wawApiKey']);
-    const map = new Map();
-    apiClient.getStops(`${options['wawApiAllStops']}${options['wawApiKey']}`)
-      .then(stopsList => storage.storeData('stopsList', stopsList));
-
-    const searchManager = new SearchManager('zbiorkom-app', () => storage.getData('stopsList'));
-    searchManager.createInput();
-    searchManager.addSelectionHandler(async (selection) => {
-        const listOfLines = await apiClient.getLines(selection.id, selection.number);
-        const stopLinesManager = new StopLinesManager('zbiorkom-app', listOfLines);
-        stopLinesManager.createLinesTable();
-    })
-    map.initializeMap();  
+    console.log("App works!");
 }
